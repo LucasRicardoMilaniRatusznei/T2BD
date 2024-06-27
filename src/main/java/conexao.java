1580bcb67ea138bca95fda27e1b0b6eb0028d115
Eduardo Soviersovski <eduardo.soviersovski@exati.com.br>
2024-06-27 20:19:00 -0300

    Adicionado comentários.

--- src/main/java/conexao.java
+++ src/main/java/conexao.java
@@ -2,6 +2,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/*
+JDBC é um conjunto de classes e interfaces (API) escritas em Java que fazem o envio de instruções SQL para qualquer
+ banco de dados relacional
+O driver JDBC fornece a conexão ao banco de dados e implementa o protocolo para transferir a consulta e o resultado
+entre cliente e banco de dados.
+ */
+
 public class conexao {
     public static void main(String[] args) throws SQLException {
         Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Treinamento",
